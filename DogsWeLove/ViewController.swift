9049c7023f2533e14acaaf4904c0bd33bf9f2db6
Carlos Linares <cadalisa@gmail.com>
2020-01-24 10:28:39 -0600

    Initial Commit

---
+++ DogsWeLove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogsWeLove
+//
+//  Created by Carlos Linares on 24/01/20.
+//  Copyright © 2020 Carlos Linares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
