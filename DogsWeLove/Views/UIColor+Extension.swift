3384f5c768f39b39d65a8fdbd5234296284aea17
Carlos Linares <cadalisa@gmail.com>
2020-01-25 09:25:12 -0600

    - Introduce colors and static variables to access

---
+++ DogsWeLove/Views/UIColor+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extension.swift
+//  DogsWeLove
+//
+//  Created by Carlos Linares on 24/01/20.
+//  Copyright © 2020 Carlos Linares. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    struct DogsWeLoveColorScheme {
+        static var background = UIColor(named: "background")
+        static var textContent = UIColor(named: "text-content")
+        static var textTitle = UIColor(named: "text-title")
+    }
+}
